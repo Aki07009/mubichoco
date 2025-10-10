c941343490d318e10bfa397e911197eacc4722d5
Aki07009 <neriyamsandesuyo@gmail.com>
2025-10-11 03:22:19 +0900

    Update wantList.js

--- js/wantList.js
+++ js/wantList.js
@@ -1,15 +1,1 @@
-let allMovies = [];
 
-async function loadWant() {
-  const api = getAPI();
-  if (!api) return setText('wantList', "API未設定");
-  const data = await getJSON(api + '?action=list');
-  if (!data) return setText('wantList', "読み込み失敗");
-
-  allMovies = data;
-  renderWantList(data);
-}
-
-function renderWantList(data) {
-  // ジャンル分類など元の処理をここへ移す
-}
