17a0541bd498635bfb0b55d5f4d21c6b4e73dcf9
Aki07009 <neriyamsandesuyo@gmail.com>
2025-10-11 00:15:54 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+let API = localStorage.getItem('mubichoco_api') || '';
+
+async function getJSON(url) {
+  try {
+    const res = await fetch(url);
+    return await res.json();
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+function setAPI(url) {
+  localStorage.setItem('mubichoco_api', url);
+  API = url;
+}
+
+function getAPI() {
+  return API;
+}
 
